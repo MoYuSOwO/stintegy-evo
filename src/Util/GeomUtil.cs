b41d59df8c2ebcb880668ca0d7fff866beb0ee42
MoYuSOwO <175416583+MoYuSOwO@users.noreply.github.com>
2026-04-27 00:05:29 +0800

    feat: add helpers

--- src/Util/GeomUtil.cs
+++ src/Util/GeomUtil.cs
@@ -20,4 +20,24 @@ public static class GeomUtil
 
         return tangent.Normalized();
     }
+
+    public static float NormalizeAngle(float angle) {
+        while (angle > Mathf.Pi) angle -= (float) (2 * Mathf.Pi);
+        while (angle < -Mathf.Pi) angle += (float) (2 * Mathf.Pi);
+        return angle;
+    }
+
+    public static float Curvature(Vector2 a, Vector2 b, Vector2 c)
+    {
+        Vector2 ab = b - a, ac = c - a, bc = c - b;
+        float cross = ab.Cross(ac);
+        if (Mathf.Abs(cross) < 1e-6f) return 0f;
+        float lenAB = ab.Length();
+        float lenAC = ac.Length();
+        float lenBC = bc.Length();
+        float product = lenAB * lenAC * lenBC;
+        if (product < 1e-6f) return 0f;
+
+        return (2 * cross) / product;
+    }
 }
